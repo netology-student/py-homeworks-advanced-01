935ea3289ff3e4b62f19c14a6341f749d6930cab
AlekseyGostev <gostev.av@gmail.com>
2022-07-28 22:19:12 -0700

    Completed

---
+++ application/db/people.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def get_employees():
+    print(f'get_employees {datetime.now()}')
---
+++ application/salary.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def calculate_salary():
+    print(f'calculate_salary {datetime.now()}')
